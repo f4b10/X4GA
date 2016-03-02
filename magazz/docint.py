4f57449dd6454648f8a5c387ca3d476e469bc224
Marcello <marcello.montaldo@gmail.com>
2016-03-02 10:37:24 +0100

    Rivista costruzione grid nell'ottica di agevolarne la personalizzazione

--- magazz/docint.py
+++ magazz/docint.py
@@ -6,17 +6,17 @@
 # Copyright:    (C) 2011 Astra S.r.l. C.so Cavallotti, 122 18038 Sanremo (IM)
 # ------------------------------------------------------------------------------
 # This file is part of X4GA
-# 
+#
 # X4GA is free software: you can redistribute it and/or modify
 # it under the terms of the Affero GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # X4GA is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with X4GA.  If not, see <http://www.gnu.org/licenses/>.
 # ------------------------------------------------------------------------------
@@ -42,12 +42,12 @@ FRAME_TITLE = "Interrogazione documenti magazzino"
 
 
 class _DocIntGridMixin(object):
-    
+
     def __init__(self):
         assert isinstance(self, dbglib.DbGrid)
         object.__init__(self)
         self.Bind(gl.EVT_GRID_CELL_LEFT_DCLICK, self.OnCallDoc)
-    
+
     def OnCallDoc(self, event):
         assert isinstance(self, dbglib.DbGrid)
         row = event.GetRow()
@@ -76,15 +76,15 @@ class _DocIntGridMixin(object):
 
 
 class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
-    
+
     def __init__(self, parent, dlg, dbdoc):
-        
+
         dbglib.DbGridColoriAlternati.__init__(self, parent,
                                               size=parent.GetSize())
         _DocIntGridMixin.__init__(self)
         self.dbdoc = dbdoc
         self.dlg = dlg
-        
+
         self.totdef = ("totimponib",
                        "totimposta",
                        "totimporto",
@@ -95,9 +95,38 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
                        "totscrip",
                        "totscmce",
                        "totomagg")
-        
+
         size = parent.GetClientSizeTuple()
+
+        #cols=self.SetColumnsGrid()
+        cols=self.GetDbColumns()
         
+        self._cols=cols
+        colmap  = [c[1] for c in cols]
+        colsize = [c[0] for c in cols]
+
+        canedit = False
+        canins = False
+
+        rs = self.dbdoc.GetRecordset()
+
+        self.SetData( rs, colmap, canedit, canins)
+        self.GetTable().dbdoc = self.dbdoc
+
+        map(lambda c:\
+            self.SetColumnDefaultSize(c[0], c[1]), enumerate(colsize))
+
+        self.SetAnchorColumns(10, 6)
+        self.AutoSizeColumns()
+        sz = wx.FlexGridSizer(1,0,0,0)
+        sz.AddGrowableCol( 0 )
+        sz.AddGrowableRow( 0 )
+        sz.Add(self, 0, wx.GROW|wx.ALL, 0)
+        parent.SetSizer(sz)
+        sz.SetSizeHints(parent)
+
+
+    def GetDbColumns(self):
         cn = lambda db, col: db._GetFieldIndex(col, inline=True)
         doc = self.dbdoc
         spe = doc.speinc
@@ -106,12 +135,12 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
         pdc = doc.pdc
         mpa = doc.modpag
         age = doc.agente
-        
+
         _STR = gl.GRID_VALUE_STRING
         _DAT = gl.GRID_VALUE_DATETIME
         _CHK = gl.GRID_VALUE_BOOL+":1,0"
         _IMP = bt.GetValIntMaskInfo()
-        
+
         cols = (\
             ( 40, (cn(mag,"codice"),     "Mag.",          _STR, True)),
             ( 80, (cn(doc,"datreg"),     "Data reg.",     _DAT, True)),
@@ -138,73 +167,50 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
             (  1, (cn(doc,"id"),         "#doc",          _STR, True)),
             (  1, (cn(doc,"id_reg"),     "#reg",          _STR, True)),
         )
-        
 
-        self._cols=cols
-        colmap  = [c[1] for c in cols]
-        colsize = [c[0] for c in cols]
-        
-        canedit = False
-        canins = False
-        
-        rs = self.dbdoc.GetRecordset()
-        
-        self.SetData( rs, colmap, canedit, canins)
-        self.GetTable().dbdoc = self.dbdoc
-        
-        map(lambda c:\
-            self.SetColumnDefaultSize(c[0], c[1]), enumerate(colsize))
-        
-        self.SetAnchorColumns(10, 6)
-        self.AutoSizeColumns()
-        sz = wx.FlexGridSizer(1,0,0,0)
-        sz.AddGrowableCol( 0 )
-        sz.AddGrowableRow( 0 )
-        sz.Add(self, 0, wx.GROW|wx.ALL, 0)
-        parent.SetSizer(sz)
-        sz.SetSizeHints(parent)
-    
+        return cols
+
     def UpdateGrid(self):
-        
+
         doc = self.dbdoc
-        
+
         wx.BeginBusyCursor()
-        
+
         try:
-            
+
             wait = aw.awu.WaitDialog(self.dlg, dataread=True)
             doc.Retrieve()
             wait.Destroy()
-            
+
             self.ChangeData(doc.GetRecordset())
             self.AutoSizeColumns()
-            
+
             def ProgrUpdate(doc, row):
                 pass
     #            wait.SetValue(row)
-            
-            totals = doc.GetTotalOf([(doc, tot) for tot in self.totdef], 
+
+            totals = doc.GetTotalOf([(doc, tot) for tot in self.totdef],
                                     ProgrUpdate)
-            
+
             cn = lambda x: self.dlg.FindWindowByName(x)
-            
+
             cn("totnumdoc").SetValue(self.dbdoc.RowsCount())
             for n, name in enumerate(self.totdef):
                 try:
                     cn(name).SetValue(totals[n])
                 except AttributeError:
                     pass
-        
+
         finally:
             wx.EndBusyCursor()
-    
+
     def GridDocOnUpdateFilters(self, event):
-        
+
         doc = self.dbdoc
         doc.ClearFilters()
-        
+
         cn = self.dlg.FindWindowByName
-        
+
         for tab, name in (("doc", "id_magazz"),\
                           ("doc", "id_pdc"),\
                           ("doc", "id_agente"),\
@@ -213,13 +219,13 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
             val = ctr.GetValue()
             if val is not None:
                 doc.AddFilter("%s.%s=%%s" % (tab, name), val)
-        
+
         doc.ClearOrders()
         doc.AddOrder('doc.datreg')
         doc.AddOrder('tipdoc.codice')
         doc.AddOrder('doc.datdoc')
         doc.AddOrder('doc.numdoc')
-        
+
         td = cn('id_tipdoc').GetValue()
         if td:
             try:
@@ -235,7 +241,7 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
                 doc.AddOrder('doc.numdoc')
             else:
                 doc.AddFilter('doc.id_tipdoc=%s', td)
-        
+
         for name in ("num", "dat"):
             for tipo in ("doc", "reg", "rif"):
                 for estr in '12':
@@ -247,7 +253,7 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
                     if val:
                         field = "doc.%s%s" % (name, tipo)
                         doc.AddFilter("%s%s%%s" % (field, oper), val)
-                        
+
        #filtro descrizione riferimento
         d = cn('masdesrif')
         if d:
@@ -257,22 +263,22 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
                 if bt.OPTSPASEARCH:
                     val = val.replace(' ', r'%')
                 doc.AddFilter('doc.desrif LIKE %s', val)
-        
+
         for name in 'ann acq'.split():
             val = cn('nodoc%s' % name).IsChecked()
             if val:
                 doc.AddFilter("(doc.f_%s IS NULL OR doc.f_%s<>1)" % (name, name))
-        
+
         self.UpdateGrid()
 
     def GridDocOnResetFilters(self):
         self.dbdoc.ClearFilters()
         self.UpdateGrid()
-    
+
     def GridDocOnPrint(self, event):
         self.Print()
         event.Skip()
-    
+
     def Print(self):
         rpt.Report(self, self.dbdoc, "Lista documenti magazzino",
                    filtersPanel=aw.awu.GetParentFrame(self).FindWindowByName('panelsel'))
@@ -282,12 +288,15 @@ class DocIntGrid(dbglib.DbGridColoriAlternati, _DocIntGridMixin):
 
 
 class DocIntPanel(aw.Panel):
-    
+
     def __init__(self, *args, **kwargs):
-        
+
         aw.Panel.__init__(self, *args, **kwargs)
-        self.dbdoc = dbm.ElencoDocum()
-        self.dbtpd = dbm.adb.DbTable(bt.TABNAME_CFGMAGDOC, 'tipdoc')
+        #=======================================================================
+        # self.dbdoc = dbm.ElencoDocum()
+        # self.dbtpd = dbm.adb.DbTable(bt.TABNAME_CFGMAGDOC, 'tipdoc')
+        #=======================================================================
+        self.SetData()
         wdr.DocMagFunc(self)
         cn = self.FindWindowByName
         pp = self.FindWindowById(wdr.ID_PANGRIDDOC)
@@ -296,7 +305,11 @@ class DocIntPanel(aw.Panel):
         for cid, func in ((wdr.ID_MASBUTUPD, self.griddoc.GridDocOnUpdateFilters),
                           (wdr.ID_MASBUTPRT, self.griddoc.GridDocOnPrint)):
             self.Bind(wx.EVT_BUTTON, func, id=cid)
-    
+
+    def SetData(self):
+        self.dbdoc = dbm.ElencoDocum()
+        self.dbtpd = dbm.adb.DbTable(bt.TABNAME_CFGMAGDOC, 'tipdoc')
+
     def OnDocChanged(self, event):
         cn = self.FindWindowByName
         td = cn('id_tipdoc')
@@ -307,7 +320,7 @@ class DocIntPanel(aw.Panel):
         cn('id_pdc').SetPdcTipCods(c)
         self.EnableFields()
         event.Skip()
-    
+
     def EnableFields(self):
         cn = self.FindWindowByName
         td = cn('id_tipdoc').dbdoc
