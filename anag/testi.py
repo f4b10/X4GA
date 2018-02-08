60bc211d4bff865cf4bc029956255fe89b65a341
Marcello <marcello.montaldo@gmail.com>
2018-02-08 18:56:23 +0100

    Aggiunta Gestione dei testi richiamabili

---
+++ anag/testi.py
@@ -0,0 +1,99 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# Name:         anag/testi.py
+# Author:       Fabio Cassini <fabio.cassini@gmail.com>
+# Copyright:    (C) 2011 Astra S.r.l. C.so Cavallotti, 122 18038 Sanremo (IM)
+# ------------------------------------------------------------------------------
+# This file is part of X4GA
+#
+# X4GA is free software: you can redistribute it and/or modify
+# it under the terms of the Affero GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# X4GA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with X4GA.  If not, see <http://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+
+import wx
+
+import MySQLdb
+
+import awc.layout.gestanag as ga
+import anag.testi_wdr as wdr
+
+import awc.util as util
+
+from Env import Azienda
+bt = Azienda.BaseTab
+
+
+FRAME_TITLE = "Testi Richiamabili"
+
+
+class TestiPanel(ga.AnagPanel):
+    def __init__(self, *args, **kwargs):
+        ga.AnagPanel.__init__(self, *args, **kwargs)
+        self.SetDbSetup( bt.tabelle[ bt.TABSETUP_TABLE_TESTI] )
+        self.db_report = None
+        #self.db_report = "Testi Richiamabili"
+
+    def InitAnagCard(self, parent):
+        p = wx.Panel( parent, -1)
+        wdr.TestiFunc( p, True )
+        return p
+
+
+# ------------------------------------------------------------------------------
+
+
+class TestiFrame(ga._AnagFrame):
+    """
+    Frame Gestione tabella Status clienti.
+    """
+    def __init__(self, *args, **kwargs):
+        if not kwargs.has_key('title') and len(args) < 3:
+            kwargs['title'] = FRAME_TITLE
+        ga._AnagFrame.__init__(self, *args, **kwargs)
+        self.LoadAnagPanel(TestiPanel(self, -1))
+
+
+# ------------------------------------------------------------------------------
+
+
+class TestiDialog(ga._AnagDialog):
+    """
+    Dialog Gestione tabella Status clienti.
+    """
+    def __init__(self, *args, **kwargs):
+        if not kwargs.has_key('title') and len(args) < 3:
+            kwargs['title'] = FRAME_TITLE
+        ga._AnagDialog.__init__(self, *args, **kwargs)
+        self.LoadAnagPanel(TestiPanel(self, -1))
+
+
+# ------------------------------------------------------------------------------
+
+
+def runTest(frame, nb, log):
+    win = TestiDialog()
+    win.Show()
+    return win
+
+
+# ------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import sys,os
+    import runtest
+    import stormdb as adb
+    db = adb.DB()
+    db.Connect()
+    runtest.main(['', os.path.basename(sys.argv[0])] + sys.argv[1:])
