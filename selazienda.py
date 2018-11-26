c5ee17659a75abaa76198649a6eb9de9545d1bdc
Marcello <marcello.montaldo@gmail.com>
2018-11-26 12:14:13 +0100

    Corretto b7ug che simula la funzione old_password di MySql.

--- selazienda.py
+++ selazienda.py
@@ -383,16 +383,17 @@ class SelAziendaPanel(aw.Panel):
 
 
     def mysql_hash_password(self, password):
+        retValue = ''
         nr = 1345345333
         add = 7
         nr2 = 0x12345671
-
-        for c in (ord(x) for x in password if x not in (' ', '\t')):
-            nr^= (((nr & 63)+add)*c)+ (nr << 8) & 0xFFFFFFFF
-            nr2= (nr2 + ((nr2 << 8) ^ nr)) & 0xFFFFFFFF
-            add= (add + c) & 0xFFFFFFFF
-
-        return "%08x%08x" % (nr & 0x7FFFFFFF,nr2 & 0x7FFFFFFF)
+        if len(password.strip())>0:
+            for c in (ord(x) for x in password if x not in (' ', '\t')):
+                nr^= (((nr & 63)+add)*c)+ (nr << 8) & 0xFFFFFFFF
+                nr2= (nr2 + ((nr2 << 8) ^ nr)) & 0xFFFFFFFF
+                add= (add + c) & 0xFFFFFFFF
+            retValue = "%08x%08x" % (nr & 0x7FFFFFFF,nr2 & 0x7FFFFFFF)
+        return retValue
 
 
 
