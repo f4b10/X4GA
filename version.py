6c53a3076a2037f9a9725fd9a8481f1e5716a8be
Marcello <marcello.montaldo@gmail.com>
2020-02-07 09:32:40 +0100

    Vers.1.5.89

--- version.py
+++ version.py
@@ -31,7 +31,7 @@ from _version import apptype, appType, appinfo
 
 VERSION_MAJOR   = 1
 VERSION_MINOR   = 5
-VERSION_RELEASE = 88
+VERSION_RELEASE = 89
 VERSION_TAG     = ""
 VERSION_TYPE    = apptype
 VERSION_TYPEXT  = appType
