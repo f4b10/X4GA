6506f54aec64cb541e8e26841a3c8ab5f5ba12be
Marcello <marcello.montaldo@gmail.com>
2020-12-17 19:00:30 +0100

    Proposizione date in debug

--- magazz/ftdif.py
+++ magazz/ftdif.py
@@ -661,8 +661,8 @@ class FtDifPanel(aw.Panel):
             d2 = today
 
         if _DEBUG:
-            d1 = dt.DateTime(2020, 7, 1)
-            d2 = dt.DateTime(2020, 7, 5)
+            d1 = dt.DateTime(2020, 12, 1)
+            d2 = dt.DateTime(2020, 12, 5)
 
 
 
