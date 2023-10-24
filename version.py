09977e02afef5c6d922fa631041bcd9a0fc808b1
marce <marce@PC-MARCELLO.Astra.local>
2023-10-24 13:46:54 +0200

    Vers. 1.6.12

--- version.py
+++ version.py
@@ -37,7 +37,7 @@ VERSION_TYPE    = apptype
 VERSION_TYPEXT  = appType
 VERSION_INFO    = appinfo
 
-__min_compat_ver__ = '1.6.11'
+__min_compat_ver__ = '1.6.12'
 
 VERSION = (VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE, VERSION_TAG)
 
